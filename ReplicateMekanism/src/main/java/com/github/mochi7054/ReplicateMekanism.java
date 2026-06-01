21c1e516d08e5caca809728ae4dfcb1ebe29df50
mochi7054 <229789565+mochi7054@users.noreply.github.com>
2026-06-02 02:22:36 +0900

    modidの修正

--- ReplicateMekanism/src/main/java/com/github/mochi7054/ReplicateMekanism.java
+++ ReplicateMekanism/src/main/java/com/github/mochi7054/ReplicateMekanism.java
@@ -69,6 +69,7 @@ public class ReplicateMekanism {
 
     // The constructor for the mod class is the first code that is run when your mod is loaded.
     // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+
     public ReplicateMekanism(IEventBus modEventBus, ModContainer modContainer) {
 
 
@@ -86,7 +87,7 @@ public class ReplicateMekanism {
         // Register ourselves for server and other game events we are interested in.
         // Note that this is necessary if and only if we want *this* class (ReplicateMekanism) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
-        NeoForge.EVENT_BUS.register(this);
+        // NeoForge.EVENT_BUS.register(this);
 
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
         modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
