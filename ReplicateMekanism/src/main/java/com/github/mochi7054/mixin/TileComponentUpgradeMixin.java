2b2a701bf466f7cfc84cca0c267c459420f60ea4
mochi7054 <229789565+mochi7054@users.noreply.github.com>
2026-06-02 02:29:28 +0900

    未サポート修正

--- ReplicateMekanism/src/main/java/com/github/mochi7054/mixin/TileComponentUpgradeMixin.java
+++ ReplicateMekanism/src/main/java/com/github/mochi7054/mixin/TileComponentUpgradeMixin.java
@@ -11,28 +11,21 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = TileComponentUpgrade.class, remap = false)
 public abstract class TileComponentUpgradeMixin {
-    @Shadow
-    private Set<Upgrade> supported;
-
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void onInit(mekanism.common.tile.base.TileEntityMekanism tile, org.spongepowered.asm.mixin.injection.callback.CallbackInfo ci) {
-        if (this.supported == null) {
-            this.supported = java.util.EnumSet.of(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE);
-        } else {
-            try {
-                this.supported.add(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE);
-            } catch (UnsupportedOperationException e) {
-                Set<Upgrade> mutable = new java.util.HashSet<>(this.supported);
-                mutable.add(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE);
-                this.supported = java.util.Collections.unmodifiableSet(mutable);
-            }
-        }
-    }
 
     @Inject(method = "supports", at = @At("HEAD"), cancellable = true)
     private void onSupports(Upgrade upgrade, CallbackInfoReturnable<Boolean> cir) {
         if (upgrade != null && upgrade.name().equals("REPLICA")) {
             cir.setReturnValue(true);
+        }
+    }
+
+    @Inject(method = "getSupportedTypes", at = @At("RETURN"), cancellable = true)
+    private void onGetSupportedTypes(CallbackInfoReturnable<Set<Upgrade>> cir) {
+        Set<Upgrade> supported = cir.getReturnValue();
+        if (supported != null && !supported.contains(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE)) {
+            Set<Upgrade> mutable = new java.util.HashSet<>(supported);
+            mutable.add(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE);
+            cir.setReturnValue(java.util.Collections.unmodifiableSet(mutable));
         }
     }
 }
