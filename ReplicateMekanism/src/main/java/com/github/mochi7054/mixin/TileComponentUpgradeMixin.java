9be16492ba548b75cad2a0d8a1ad560572533cfe
みちゃもち <229789565+mochi7054@users.noreply.github.com>
2026-08-26 17:47:01 +0900

    Award cheated replica advancement when installing upgrades via machine GUI (TileComponentUpgrade.addUpgrades)

--- ReplicateMekanism/src/main/java/com/github/mochi7054/mixin/TileComponentUpgradeMixin.java
+++ ReplicateMekanism/src/main/java/com/github/mochi7054/mixin/TileComponentUpgradeMixin.java
@@ -12,6 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = TileComponentUpgrade.class, remap = false)
 public abstract class TileComponentUpgradeMixin {
 
+    @Shadow
+    private mekanism.common.tile.base.TileEntityMekanism tile;
+
     @Inject(method = "supports", at = @At("HEAD"), cancellable = true)
     private void onSupports(Upgrade upgrade, CallbackInfoReturnable<Boolean> cir) {
         if (upgrade != null && upgrade.name().equals("REPLICA")) {
@@ -26,6 +29,20 @@ public abstract class TileComponentUpgradeMixin {
             Set<Upgrade> mutable = new java.util.HashSet<>(supported);
             mutable.add(com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE);
             cir.setReturnValue(java.util.Collections.unmodifiableSet(mutable));
+        }
+    }
+
+    @Inject(method = "addUpgrades(Lmekanism/api/Upgrade;I)I", at = @At("RETURN"))
+    private void onAddUpgrades(Upgrade upgrade, int installed, CallbackInfoReturnable<Integer> cir) {
+        if (upgrade == com.github.mochi7054.ReplicateMekanism.REPLICA_UPGRADE_TYPE) {
+            if (this.tile != null && this.tile.getLevel() instanceof net.minecraft.server.level.ServerLevel serverLevel) {
+                var pos = this.tile.getBlockPos();
+                for (net.minecraft.server.level.ServerPlayer player : serverLevel.players()) {
+                    if (player.distanceToSqr(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5) <= 256.0) {
+                        com.github.mochi7054.ReplicateMekanism.checkAndAwardCheatedAdvancement(player);
+                    }
+                }
+            }
         }
     }
 }
